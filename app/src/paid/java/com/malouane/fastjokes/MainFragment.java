d76aec4d88821edefae5b2873680b7baa0528521
Mohamed Alouane <flahweb@gmail.com>
2018-02-05 20:28:53 +0000

    remove ads in paid flavor, make progressbar gone when app starts

--- app/src/paid/java/com/malouane/fastjokes/MainFragment.java
+++ app/src/paid/java/com/malouane/fastjokes/MainFragment.java
@@ -5,8 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 
 public class MainFragment extends Fragment {
 
@@ -16,11 +14,6 @@ public class MainFragment extends Fragment {
   @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     View root = inflater.inflate(R.layout.fragment_main, container, false);
-
-    AdView mAdView = root.findViewById(R.id.adView);
-    AdRequest adRequest =
-        new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
-    mAdView.loadAd(adRequest);
     return root;
   }
 }
